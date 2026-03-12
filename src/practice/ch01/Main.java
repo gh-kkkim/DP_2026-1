cd2f9c70b5d0cb91d1ac563600225a15d40cba99
김가현 <ujm10460@naver.com>
2026-03-12 11:46:48 +0900

    3/12

--- src/practice/ch01/Main.java
+++ src/practice/ch01/Main.java
@@ -1,8 +1,36 @@
 package practice.ch01;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
+        // 책 테스트
         Book book = new Book("Java Programming");
         System.out.println("Book name: " + book.getName());
+
+        // 책장 테스트
+        BookShelf bookShelf = new BookShelf(10);
+        bookShelf.appendBook(new Book("Java Programming"));
+        bookShelf.appendBook(new Book("Design Patterns"));
+        bookShelf.appendBook(new Book("Effective Java"));
+
+        // 책장에 있는 책 출력
+
+        // 방법1: 클라이어트가 책꽂이에 접근하여 책을 하나씩 가져오는 방법
+        for (int i = 0; i < bookShelf.getLength(); i++) {
+            System.out.println("Book" + (i + 1) + ": " + bookShelf.getBookAt(i).getName());
+        }
+
+        // 방법2: interator를 이용하여 책을 하나씩 가져오는 방법
+        // iterator를 얻어옴
+        Iterator<Book> it = bookShelf.iterator();
+        while(it.hasNext()) {
+            System.out.println("Book: " + it.next().getName());
+        }
+
+        // 확장 for문을 이용한 방법
+        // 이게 가능한 이유는 BookShelf가 Iterable 인터페이스를 구현했기 때문
+        for (Book b : bookShelf) { //형식: for (요소의 타입 변수명 : 컬렉션) { ... }
+            System.out.println("Book: " + b.getName());
+        }
     }
 }
