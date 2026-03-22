1f413fa38ece7fa0c3c6bfe1c28edbd7fca7422a
김가현 <ujm10460@naver.com>
2026-03-22 21:31:43 +0900

    1장 과제 완료

---
+++ src/hw/ch01/BookShelfGenreIterator.java
@@ -0,0 +1,38 @@
+package hw.ch01;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BookShelfGenreIterator implements Iterator<Book> {
+    private BookShelf bookShelf;
+    private String genre;
+    private int index;
+
+    public BookShelfGenreIterator(BookShelf bookShelf, String genre) {
+        this.bookShelf = bookShelf;
+        this.genre = genre;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (index < bookShelf.getLength()) {
+            Book book = bookShelf.getBookAt(index);
+            if (book != null && genre != null && genre.equals(book.getGenre())) {
+                return true;
+            }
+            index++;
+        }
+        return false;
+    }
+
+    @Override
+    public Book next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        Book nextBook = bookShelf.getBookAt(index);
+        index++;
+        return nextBook;
+    }
+}
---
+++ src/hw/ch01/BookShelfYearIterator.java
@@ -0,0 +1,37 @@
+package hw.ch01;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class BookShelfYearIterator implements Iterator<Book> {
+    private final List<Book> sortedBooks;
+    private int index;
+
+    public BookShelfYearIterator(BookShelf bookShelf) {
+        this.sortedBooks = new ArrayList<>();
+        for (int i = 0; i < bookShelf.getLength(); i++) {
+            Book book = bookShelf.getBookAt(i);
+            if (book != null) {
+                sortedBooks.add(book);
+            }
+        }
+        this.sortedBooks.sort(Comparator.comparingInt(Book::getYear).reversed());
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < sortedBooks.size();
+    }
+
+    @Override
+    public Book next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return sortedBooks.get(index++);
+    }
+}
