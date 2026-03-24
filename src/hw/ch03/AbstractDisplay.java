7670a5ae72c38d1321263f027f97dc60b3207c91
김가현 <ujm10460@naver.com>
2026-03-24 15:55:24 +0900

    3장 과제 완료

---
+++ src/hw/ch03/AbstractDisplay.java
@@ -0,0 +1,28 @@
+package hw.ch03;
+
+public abstract class AbstractDisplay {
+    // open, print, close는 하위 클래스에 구현을 맡기는 추상 메소드 
+    private int repeatCount;
+
+    public AbstractDisplay() {
+        this(5);
+    }
+
+    public AbstractDisplay(int repeatCount) {
+        this.repeatCount = repeatCount;
+    }
+
+    public abstract void open();
+    public abstract void print();
+    public abstract void close();
+
+    // display는 AbstractDisplay에서 구현하는 메소드
+    // 큰 틀(흐름, 구조)을 제공함 => 템플릿 메소드 
+    public final void display() {
+        open();
+        for (int i = 0; i < repeatCount; i++) {
+            print();
+        }
+        close();
+    }
+}
