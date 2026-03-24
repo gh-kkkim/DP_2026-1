fe5ac31386a5cf8d819bb1c3ca3534c10d0fa40a
김가현 <ujm10460@naver.com>
2026-03-24 10:15:12 +0900

    3/24

---
+++ src/practice/ch03/MAin.java
@@ -0,0 +1,16 @@
+package practice.ch03;
+
+public class MAin {
+    public static void main(String[] args) {
+        //CharDisplay d1 = new CharDisplay('H'); //'H' char, "H" String
+        AbstractDisplay d1 = new CharDisplay('H');
+        d1.display();
+
+        // StringDisplay d2 = new StringDisplay("Hello, world.");
+        AbstractDisplay d2 = new StringDisplay("Hello, world.");
+        d2.display();
+
+        // AbstractDisplay d3 = new AbstractDisplay();
+        // 추상 클래스는 인스턴스를 생성할 수 없다.
+    }
+}
