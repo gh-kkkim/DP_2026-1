e4207c4290beb24a78c7ed7c64ba165d8e60380a
김가현 <ujm10460@naver.com>
2026-04-21 09:46:00 +0900

    4/21

--- src/practice/ch11/Main.java
+++ src/practice/ch11/Main.java
@@ -3,14 +3,19 @@ package practice.ch11;
 public class Main {
     public static void main(String[] args) {
         // 디렉토리 트리를 만든다
-        Directory root = new Directory("root");
+        // Directory root = new Directory("root");
+
+        Entry root = new Directory("root"); // 업캐스닝은 자동으로 일어난다.
+
         Directory bin = new Directory("bin");
         Directory tmp = new Directory("tmp");
         Directory usr = new Directory("usr");
 
-        root.add(bin);
-        root.add(tmp);
-        root.add(usr);
+        ((Directory)root).add(bin);
+        // 다운캐스팅
+        // . 연산자가 타입캐스팅 연산자보다 우선순위가 높다.
+        ((Directory)root).add(tmp);
+        ((Directory)root).add(usr);
 
         // 파일 생성 및 추가
         bin.add(new File("vi", 10000));
