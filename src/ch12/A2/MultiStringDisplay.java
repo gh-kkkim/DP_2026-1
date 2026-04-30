aaf5a812daf62e8731fa650e54de0fb7d1be8e56
김가현 <ujm10460@naver.com>
2026-04-30 11:39:29 +0900

    4/30

--- src/ch12/A2/MultiStringDisplay.java
+++ src/ch12/A2/MultiStringDisplay.java
@@ -7,16 +7,16 @@ public class MultiStringDisplay extends Display {
     // 표시 문자열 저장 장소
     private List<String> body = new ArrayList<>();
     // 표시 문자열 최대 문자 수 
-    private int columns = 0;
+    private int columns = 0; // 현재 제일 긴 문자열의 길이
 
     // 문자열 추가 
     public void add(String msg) {
         body.add(msg);
-        if (columns < msg.length()) {
+        if (columns < msg.length()) { // 더 긴 문자열이 추가되면...
             // 최대 문자 수 갱신
             columns = msg.length();
         }
-        updatePadding();
+        updatePadding(); // 제일 긴 문자에 맞춰서 나머지 문자열의 오른쪽 끝에 채울 공백을 늘린다
     }
 
     @Override
@@ -36,10 +36,11 @@ public class MultiStringDisplay extends Display {
 
     // 표시 문자열 오른쪽 끝에 채울 공백을 필요에 따라 늘린다
     private void updatePadding() {
+    // 현재 추가된 모든 문자열에 대해서 for문을 돌면서 부족한 만큼 공백을 추가한다
         for (int row = 0; row < body.size(); row++) {
             String line = body.get(row);
             int padding = columns - line.length();
-            if (padding > 0) {
+            if (padding > 0) { // 현재 문자열이 제일 긴 문자열보다 짧으면...
                 body.set(row, line + spaces(padding));
             }
         }
