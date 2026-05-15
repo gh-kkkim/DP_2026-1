f78d65f8207e0cefb50862d0ef78305b6a43f83f
김가현 <ujm10460@naver.com>
2026-05-15 16:24:25 +0900

    16장 과제 완료

---
+++ src/hw/ch16/Main.java
@@ -0,0 +1,8 @@
+package hw.ch16;
+
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("학번: 20240964 이름: 김가현");
+        new LoginFrame("Mediator Sample");
+    }
+}
