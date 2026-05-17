994a0440fef72ec2e5fb16cc95927dd1169517bd
김가현 <ujm10460@naver.com>
2026-05-17 13:00:10 +0900

    17장 과제 완료

--- src/hw/ch17/EvenNumberGenerator.java
+++ src/hw/ch17/EvenNumberGenerator.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EvenNumberGenerator extends NumberGenerator {
-    private int number;	// 현재 수 
-    private int end;		// 종료값(이 값은 포함하지 않는다) 
+    private int number;	    // 짝수여야 한다
+    private int end;		// 이 값은 포함하지 않는다
 
     public EvenNumberGenerator(int start, int end) {
         this.number = start;
