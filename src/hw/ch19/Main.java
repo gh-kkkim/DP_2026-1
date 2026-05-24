8e91c63054612e23f5ece0847c18296d7ec8a85f
김가현 <ujm10460@naver.com>
2026-05-25 00:31:18 +0900

    19장 과제 완료

--- src/hw/ch19/Main.java
+++ src/hw/ch19/Main.java
@@ -2,6 +2,7 @@ package hw.ch19;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("2024964 김가현");
         SafeFrame frame = new SafeFrame("State Sample");
         while (true) {
             for (int hour = 0; hour < 24; hour++) {
