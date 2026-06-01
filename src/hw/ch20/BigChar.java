63e094528b26b62853d03e93994481c5a9021da3
김가현 <ujm10460@naver.com>
2026-06-01 17:40:02 +0900

    20장 과제 완료

--- src/hw/ch20/BigChar.java
+++ src/hw/ch20/BigChar.java
@@ -5,18 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class BigChar {
-    static final String RED = "\u001B[31m";
-    static final String GREEN = "\u001B[32m";
-    static final String YELLOW = "\u001B[33m";
-    static final String BLUE = "\u001B[34m";
-    static final String MAGENTA = "\u001B[35m";
-    static final String CYAN = "\u001B[36m";
-    static final String RESET = "\u001B[0m";
-
     // 문자의 이름
     private char charname;
     // 큰 문자를 표현하는 문자열('#' '.' '\n'으로 이루어진 열)
     private String fontdata;
+    
+    private static final String RESET = "\u001B[0m";
 
     // 생성자
     public BigChar(char charname) {
